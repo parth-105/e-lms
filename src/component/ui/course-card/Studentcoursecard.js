734ac7ad8b3d861b8f13bfaf4d5ca6ece348f10a
parth-105 <kalathiyaharsh7@gmail.com>
2024-10-12 15:13:12 +0530

    Update Studentcoursecard.js

--- src/component/ui/course-card/Studentcoursecard.js
+++ src/component/ui/course-card/Studentcoursecard.js
@@ -64,10 +64,10 @@ export default function Studentcoursecard({
 
     const handleCourseClick = async (courseId) => {
 
-        //route.push(`/course/videos/${courseId}`);courseId, userId
+        //route.push(`/course/Videos/${courseId}`);courseId, userId
 
         if (data._id === instructor || userId || data.isAdmin) {
-            route.push(`/course/videos/${courseId}`);
+            route.push(`/course/Videos/${courseId}`);
         }
         else {
             const res = await axios.post("/api/course/checkpurchase", { userId: data._id, courseId: courseId });
@@ -114,7 +114,7 @@ export default function Studentcoursecard({
                 };
                 check()
             } else {
-                route.push(`/course/videos/${courseId}`);
+                route.push(`/course/Videos/${courseId}`);
             }
         }
     };
