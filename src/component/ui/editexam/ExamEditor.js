3febd32e64e6d2c3496e47978159f2cbb5f2f919
parth-105 <kalathiyaharsh7@gmail.com>
2024-10-12 18:48:04 +0530

    Update component/ui/editexam/ExamEditor.js

--- src/component/ui/editexam/ExamEditor.js
+++ src/component/ui/editexam/ExamEditor.js
@@ -24,7 +24,7 @@ import { Pencil, Trash2, Upload } from "lucide-react"
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+
 
 import AddEditQuestion from "@/component/quiz/addeditquestion"
 
