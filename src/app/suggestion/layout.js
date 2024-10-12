0851d3f3f2ba94fb33e88b4655a1b995e754e5ef
parth-105 <kalathiyaharsh7@gmail.com>
2024-10-12 14:50:00 +0530

    Update SGlayout.js

--- src/app/suggestion/layout.js
+++ src/app/suggestion/layout.js
@@ -12,7 +12,9 @@ export default function Layout({ children }) {
 
     return (
         <>
-            <LMSsidebar children={children} />
+           <LMSsidebar>
+        {children}
+      </LMSsidebar>
         </>
     )
 }
